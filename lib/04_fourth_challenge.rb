c807254066d818256d63aff53f3a672c07607c2b
erin dougher <ejdougher@gmail.com>
2020-11-26 07:30:20 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -5,11 +5,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        { :name => "Benvolio",
+        {:name => "Benvolio",
           :age => "17",
           :attitude => "worried"
         },
-        { :name => "Murcutio",
+        {:name => "Murcutio",
           :age => "18",
           :attitude => "hot-headed"
         }
@@ -20,11 +20,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-      { :name => "Steven",
+      {:name => "Steven",
         :age => "30",
         :attitude => "confused"
       },
-      { :name => "Nurse",
+      {:name => "Nurse",
         :age => "44",
         :attitude => "worried" 
       }
